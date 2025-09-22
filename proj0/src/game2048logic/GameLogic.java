e87d257989a13c8842c511e1aca54b7b4d2ab9ca
Lin_2z <lin1016344779@gmail.com>
2025-09-23 01:29:23 +0800

    proj0: Task6: Implements upward tilt via column iteration
    Implements the previously placeholder upward tilt by iterating over each column and delegating to the column tilt helper. Enables upward move behavior and reuses existing column logic, completing the task 6 implementation.

--- proj0/src/game2048logic/GameLogic.java
+++ proj0/src/game2048logic/GameLogic.java
@@ -78,6 +78,9 @@ public class GameLogic {
      */
     public static void tiltUp(int[][] board) {
         // TODO: fill this in in task 6
+        for (int c = 0; c < board.length; c++) {
+            tiltColumn(board, c);
+        }
         return;
     }
 
